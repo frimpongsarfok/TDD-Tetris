728a18bc31fbb9c44e1159ecf696929c1399bcff
Sarfo Kantanka Frimpong <sarfokantankafrimpong@gmail.com>
2022-02-07 17:40:15 -0500

    TTD_Tetris Solution

--- src/Board.mjs
+++ src/Board.mjs
@@ -5,9 +5,55 @@ export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.block1=undefined;
+    this.block2=undefined;
+    this.ticks=0;
+    this.string=`...\n...\n...\n`;
+    this.isAtBottom=false;
+ 
   }
 
   toString() {
-    return `TODO`;
+    
+    return this.string;
+  }
+  drop(block){
+    if(this.ticks===0 && this.block1===undefined){
+      this.block1=block;
+      this.string=`.${this.block1.color}.\n...\n...\n`;
+    
+    }else if(this.ticks!==2){
+      block.string=this.string;
+      throw new Error('already falling');
+      
+    }else if(this.ticks===2){
+      this.block2=block;
+      this.string=`.${this.block2.color}.\n...\n.${this.block1.color}.\n`;
+      this.ticks=1;
+      
+    }
+ 
+  }
+  hasFalling(){
+   // console.log(this.ticks)
+    return !this.isAtBottom;
+  }
+  tick(){
+    if(this.ticks===0){
+      this.string=`...\n.${this.block1.color}.\n...\n`;
+      ++this.ticks;
+    }else if(this.ticks===1 &&this.block2===undefined ){
+      this.string=`...\n...\n.${this.block1.color}.\n`;
+      ++this.ticks;
+    }else if(this.ticks===2 &&this.block2 ===undefined){
+      this.isAtBottom=!this.isAtBottom;
+    }else if(this.ticks===1 &&this.block2){
+      this.isAtBottom=!this.isAtBottom;
+      this.string=`...\n.${this.block2.color}.\n.${this.block1.color}.\n`;
+    }
+        
+   
   }
 }
+
+
---
+++ src/vendinemachine.test.js
@@ -0,0 +1,14 @@
+import { TestWatcher } from '@jest/core';
+import VendingMachine from './VendingMachine.js';
+
+let vendingMachine = new VendingMachine(); // VendingMachine instance
+
+describe('the vending machine', () => {
+  // YOUR TEST CASES GO HERE
+  
+    
+  test("",()=>{
+      
+  }) 
+
+});
